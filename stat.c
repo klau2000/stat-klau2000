6d0ea0a4fa73acd77267feaa2bd5167da32b9405
klau2000 <klau00@stuy.edu>
2019-11-13 21:54:39 -0500

    Changes

--- stat.c
+++ stat.c
@@ -7,15 +7,38 @@
 #include <string.h>
 //#include "rand.h"
 
+void readable(long size) {
+  int count = 0;
+  while (size >= 1000) {
+    size = size / 1000;
+    count += 1;
+    if (count == 3) {
+      break;
+    }
+  }
+  if (count == 0) {
+    printf("Readable Size: %dB\n", size);
+  }
+  if (count == 1) {
+    printf("Readable Size:%dKB\n", size);
+  }
+  if (count == 2){
+    printf("Readable Size:%dMB\n", size);
+  }
+  if (count == 3){
+    printf("Readable Size:%dGB\n", size);
+  }
+}
+
 int main() {
   struct stat test;
   stat("stat.c", &test);
-
   if(errno){ //see if any error
     printf("Error number: %d\n", errno);
     printf("Error message: %s\n", strerror(errno));
   }
-  printf("Size of this file: %ld\n", test.st_size);
+  printf("Size of this file: %ld bytes\n", test.st_size);
+  readable(test.st_size);
   printf("Mode(Permissions) of this file: %o\n", test.st_mode);
   printf("Time Last Accessed: %s\n", ctime(&test.st_atime));
 }
